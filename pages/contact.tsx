b2fbd6631ae7d5d325b0b9aeb927712fafa0cd52
robfarb <=>
2023-06-10 22:12:34 -0700

    Add contact info

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,15 +8,18 @@ export default function Home() {
     >
       <Navbar/>
       <div className="relative flex flex-col justify-center align-center h-screen">
-        <h2 className="center mb-10 text-xl text-center">Coming Soon!</h2>
-        <Image
-          className="home-logo"
-          src="/logo.png"
-          alt="Mirman Therapy Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <div className='flex flex-col items-center justify-center text-center text-xl font-montserrat'>
+          <p className='m-4'><span className='font-bold'>Phone Number:</span> (424) 248-9453</p>
+          <p className='m-4 whitespace-nowrap'><span className='font-bold'>Email:</span> drewmirmantherapy@gmail.com</p>
+          <Image
+            className="mt-12"
+            src="/logo.png"
+            alt="Mirman Therapy Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
       </div>
     </div>
   )
